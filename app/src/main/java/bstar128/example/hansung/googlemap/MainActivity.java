126e18a708defc10d8b84c3fb560d119abb2af13
StarryPurp <starbloom128@gmail.com>
2017-06-26 22:14:56 +0900

    #4 Google Map Satellite and ZoomInOut

--- app/src/main/java/bstar128/example/hansung/googlemap/MainActivity.java
+++ app/src/main/java/bstar128/example/hansung/googlemap/MainActivity.java
@@ -3,11 +3,29 @@ package bstar128.example.hansung.googlemap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+    GoogleMap googlemap;
+    SupportMapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mapFragment=(SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googlemap=googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.524088,126.918927),17));
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
     }
 }
